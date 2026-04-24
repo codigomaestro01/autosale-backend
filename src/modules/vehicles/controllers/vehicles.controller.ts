1bcb9d8e02d215bed1e1ee751f5738b014853888
codigo maestro <programadorcodigomaestro@gmail.com>
2026-04-24 09:44:58 -0600

    feat: Add first endpoint

--- src/modules/vehicles/controllers/vehicles.controller.ts
+++ src/modules/vehicles/controllers/vehicles.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { VehiclesService } from '../services/vehicles.service';
+import { CreateVehicleDto } from '../dto/vehicle.dto';
 
 @Controller('vehicles')
-export class VehiclesController {}
+export class VehiclesController {
+  constructor(private readonly vehiclesService: VehiclesService) {}
+
+  @Get()
+  getVehiclesAll() {
+    return 'Lista de vehiculos';
+  }
+
+  @Post()
+  createVehicle(@Body() createVehicleDto: CreateVehicleDto) {
+    return this.vehiclesService.create(createVehicleDto);
+  }
+}
